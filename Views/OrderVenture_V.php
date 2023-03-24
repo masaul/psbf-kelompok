42c77ef6346e43e64d93de6d6cc220c4f3c8549c
Muhammad Aulia Faqihuddin <79826454+masaul@users.noreply.github.com>
2023-03-24 23:08:31 +0700

    Views

---
+++ Views/OrderVenture_V.php
@@ -0,0 +1,26 @@
+<html>
+<head></head>
+<body>
+    <h1>Tabel Users</h1>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>Level</th>
+            <th>Username</th>
+            <th>email</th>
+            <th>pass</th>
+            <th>no_telp</th>
+            <th>alamat</th>
+        </tr>
+        <tr>
+            <td><?= $nama; ?></td>
+            <td><?= $level; ?></td>
+            <td><?= $username; ?></td>
+            <td><?= $email; ?></td>
+            <td><?= $pass; ?></td>
+            <td><?= $no_telp; ?></td>
+            <td><?= $alamat; ?></td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ Views/transaksi.php
@@ -0,0 +1,18 @@
+
+<h1>Tabel Transaksi</h1>
+    <table border="1">
+        <tr>
+            <th>ID Pesanan</th>
+            <th>ID User</th>
+            <th>Metode Bayar</th>
+            <th>Opsi Kirim</th>
+            <th>tanggal</th>
+        </tr>
+        <tr>
+            <td><?= $id_pesanan; ?></td>
+            <td><?= $id_user; ?></td>
+            <td><?= $metode_bayar; ?></td>
+            <td><?= $opsi_kirim; ?></td>
+            <td><?= $tgl_pesan; ?></td>
+        </tr>
+    </table>
